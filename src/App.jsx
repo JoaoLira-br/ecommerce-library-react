2a8a8c2dea5a8b7c0739c19f4162e4b33103af2a
Joao Victor Lira <lira440440@gmail.com>
2024-10-08 17:39:39 -0400

    hello world initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './index.css';
+import Nav from './components/Nav.jsx';
+
+
+
+
+function App() {
+  return (
+    <div className='App'>
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
+// these: need to add to index.js to use fontawesome as well as child classes like Nav.jsx
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { } from '@fortawesome/free-brands-svg-icons'
+import { faBars, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt, faSpinner, faArrowLeft, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faBars, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt, faSpinner, faArrowLeft, faShoppingCart)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+
     <App />
-  </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
